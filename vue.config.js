872ffd9e1e722dd01cd79082b757598a6da17c46
forestfrost <1355807541@qq.com>
2022-03-29 15:03:39 +0800

    feat:打包配置

--- vue.config.js
+++ vue.config.js
@@ -35,4 +35,31 @@ module.exports = defineConfig({
       title: "提示",
     },
   },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "com.electron.app",
+        productName: "工作台",
+        copyright: "Copyright © 2022",
+        win: {
+          icon: "./public/appIcon.ico",
+          target: [
+            {
+              target: "nsis",
+              arch: [
+                "x64", //64位
+                "ia32", //32位
+              ],
+            },
+          ],
+        },
+        nsis: {
+          oneClick: false, //一键安装
+          language: "2052", //安装语言
+          perMachine: true, //应用所有用户
+          allowToChangeInstallationDirectory: true, //用户可以选择路径
+        },
+      },
+    },
+  },
 });
